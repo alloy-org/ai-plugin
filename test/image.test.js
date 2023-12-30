6b743ca7cb259dbc7107ea63ef404e18a3f899b6
Bill Harding <wbharding@gmail.com>
2023-12-29 22:42:21 -0800

    WIP image test

--- test/image.test.js
+++ test/image.test.js
@@ -1,5 +1,6 @@
 import { jest } from "@jest/globals"
 import { IMAGE_FROM_PRECEDING_LABEL } from "../lib/constants/settings"
+import { DALL_E_DEFAULT, DALL_E_TEST_DEFAULT } from "../lib/constants/provider"
 import { mockPlugin, mockAppWithContent } from "./test-helpers.js"
 
 const AWAIT_TIME = 30000;
@@ -16,20 +17,14 @@ describe("with a mocked app", () => {
     app.prompt.mockImplementation((title, options) => {
       const chosenValue = options.inputs[0].value || options.inputs[0].options[0].value;
       console.debug("Chosen value", chosenValue, "from", title);
-      return chosenValue
-    })
+      return chosenValue === DALL_E_DEFAULT ? DALL_E_TEST_DEFAULT : chosenValue;
+    });
     const result = await plugin.insertText[IMAGE_FROM_PRECEDING_LABEL](app);
-    app.prompt = (title, options) => {
-      const inputs = options.inputs;
-      expect(inputs).toBeInstanceOf(Array);
-      expect(inputs).toHaveLength(1);
-      const selectInput = inputs[0];
-      const selectedInputOptions = selectInput.options;
-      expect(selectedInputOptions).toHaveLength(3);
-      const selectedOption = selectedInputOptions[0];
-      return selectedOption.value;
-    }
 
+    const tuple = app.prompt.mock.calls[app.prompt.mock.calls.length - 1];
+    const answers = tuple[1].inputs[0].options;
+    expect(answers.length).toBeGreaterThanOrEqual(1);
+    expect(answers[0].image).toContain("https://");
     expect(/!\[image]\(http/.test(result)).toBeTruthy();
   }, AWAIT_TIME);
 
