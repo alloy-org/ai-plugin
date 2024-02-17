4bb5e8f2d6eb986172407c45cfad3e338cf434df
Bill Harding <wbharding@gmail.com>
2024-02-17 15:46:53 -0800

    Checkpoint streaming trials

--- test/stream.test.js
+++ test/stream.test.js
@@ -1,13 +1,38 @@
+import fs from "fs"
 import { STREAM_MOCK_MODEL } from "../lib/constants/provider"
 import { AI_MODEL_LABEL } from "../lib/constants/settings"
 import nock from "nock"
 import path from "path"
-import { jest } from "@jest/globals"
+import { Readable } from "stream"
 import { mockAlertAccept, mockAppWithContent, mockPlugin } from "./test-helpers"
 import { fileURLToPath } from "url"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const AWAIT_TIME = 20000;
+
+class TestReadableStream extends Readable {
+  constructor(options) {
+    super(options);
+    this.data = []; // This will store the data to be streamed
+  }
+
+  _read(size) {
+    // This method is called when the stream wants to read data.
+    // You can push data into the stream here.
+    if (this.data.length >  0) {
+      this.push(this.data.shift());
+    } else {
+      this.push(null); // Signal that no more data will be provided
+    }
+  }
+
+  // Method to add data to the stream
+  addData(data) {
+    this.data.push(data);
+  }
+}
 
 // --------------------------------------------------------------------------------------
 describe("This here plugin", () => {
@@ -23,14 +48,23 @@ describe("This here plugin", () => {
     nock("https://api.openai.com")
       .persist()
       .post("/v1/chat/completions")
-      .replyWithFile(200, path.join(__dirname, "fixtures/openai-thesaurus-stream.ndjson"), {
-        "Transfer-Encoding": "chunked",
-        "X-Content-Type-Options": "nosniff",
-      }
-    );
+      .reply((uri, requestBody, cb) => {
+        const fileData = fs.readFileSync(path.join(__dirname, "fixtures/openai-thesaurus-stream.ndjson"), "utf8");
+        const readableStream = new TestReadableStream();
+        const jsonArray = JSON.parse(fileData);
+        for (const chunk of jsonArray) {
+          readableStream.addData(chunk);
+        }
+        console.log("Nock intercepted request. Sending back", fileData.length, "bytes")
+        cb(null, [ 200, "taco" ]);
+        // cb(null, [ 200, readableStream ]);
+        // return new Promise((resolve, reject) => resolve(fileData));
+        // return fs.createReadStream(path.join(__dirname, "fixtures/openai-thesaurus-stream.ndjson"));
+      });
 
     app.notes.find.mockReturnValue(note);
     mockAlertAccept(app);
+    // app.settings[AI_MODEL_LABEL] = "gpt-4-1106-preview";
     app.settings[AI_MODEL_LABEL] = STREAM_MOCK_MODEL;
     await plugin.replaceText["Thesaurus"].run(app, "manager");
 
@@ -38,5 +72,5 @@ describe("This here plugin", () => {
     const answers = tuple[1].inputs[0].options.map(option => option.value.toLowerCase());
 
     expect(answers).toEqual(["Jesus"]);
-  });
+  }, AWAIT_TIME);
 })
