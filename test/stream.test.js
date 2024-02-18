f3774d37f10a973ec54879ca12174edd100275bd
Bill Harding <wbharding@gmail.com>
2024-02-18 08:33:41 -0800

    Checkpoint streaming trials

--- test/stream.test.js
+++ test/stream.test.js
@@ -3,7 +3,6 @@ import { STREAM_MOCK_MODEL } from "../lib/constants/provider"
 import { AI_MODEL_LABEL } from "../lib/constants/settings"
 import { jest } from "@jest/globals"
 import path from "path"
-import { Readable } from "stream"
 import { mockAlertAccept, mockAppWithContent, mockPlugin } from "./test-helpers"
 import { fileURLToPath } from "url"
 
@@ -11,45 +10,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const AWAIT_TIME = 20000;
-
-class TestReadableStream extends Readable {
-  constructor(options) {
-    super(options);
-    this.data = []; // This will store the data to be streamed
-  }
-
-  _read(size) {
-    // This method is called when the stream wants to read data.
-    // You can push data into the stream here.
-    if (this.data.length >  0) {
-      this.push(this.data.shift());
-    } else {
-      this.push(null); // Signal that no more data will be provided
-    }
-  }
-
-  // Method to add data to the stream
-  addData(data) {
-    this.data.push(data);
-  }
-}
-
 const fileData = fs.readFileSync(path.join(__dirname, "fixtures/openai-thesaurus-stream.ndjson"), "utf8");
 
 function mockFetch(data) {
+  let readIndex = -1;
   return jest.fn(() =>
     Promise.resolve({
       body: {
         on: (event, handler) => {
           if (event === 'readable') {
-            console.log("Calling readable");
-            // Simulate asynchronous data chunks
-            setImmediate(() => handler());
+            handler();
           }
         },
         read: () => {
           // Return a chunk of data; simulate end of data by eventually returning null
-          return "data chunk"; // Replace with actual data you want to mock
+          readIndex += 1;
+          return fileData.split("\n")[readIndex];
         }
       },
       ok: true,
