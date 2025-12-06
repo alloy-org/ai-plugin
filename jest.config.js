744572beaeb7d1aefc095d48cf7b64b541014689
Bill Harding <wbharding@gmail.com>
2025-12-06 08:34:34 -0800

    Get tests running again

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,13 @@ dotenv.config();
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.js$': ['ts-jest', {
+      useESM: true,
+    }],
+  },
 };
